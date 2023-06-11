0fe88a82211c187c1dcc5daebdec6fd3c584117d
BonandinFrancesco <bonandinfrancesco@gmail.com>
2023-06-11 16:56:56 +0200

    migliorato un pochino il layout

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ let classNumber='101'
 
 userName = prompt( 'Ciao, mi dici il tuo nome?');
 
-console.log(userName, typeof userName);
+// console.log(userName, typeof userName);
 
 userSurname = prompt( `Bel nome.
 ti va di dirmi il tuo cognome?`);
 
-console.log(userSurname, typeof userSurname);
+// console.log(userSurname, typeof userSurname);
 
 userFavouriteColor = prompt( `Ok, quasi fatto...
 un ultima domandina, colore preferito?`);
 
-console.log(userFavouriteColor, typeof userFavouriteColor);
+// console.log(userFavouriteColor, typeof userFavouriteColor);
 
 document.getElementById('password-display')
 .innerHTML = userName + userSurname + userFavouriteColor + classNumber;
